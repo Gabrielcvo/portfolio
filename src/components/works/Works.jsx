814559e831e150bc28196611e7aa700d9efe0a02
Gabriel Santiago <gsantiagosilva.gs@gmail.com>
2022-02-02 18:13:42 -0300

    feat: works implementado e funcional

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,85 @@
+import { useState } from "react";
+
+import arrow from "../../assets/rightArrow.png";
+import mobile from "../../assets/mobile.png";
+
 import "./works.scss";
 
 export function Works() {
-  return <div className="works" id="works"></div>;
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const data = [
+    {
+      id: "1",
+      icon: mobile,
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((data) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={data.icon} />
+                  </div>
+                  <h2>{data.title}</h2>
+                  <p>{data.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+
+              <div className="right">
+                <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt="works image"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src={arrow}
+        alt="arrow left"
+        className="arrow left"
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src={arrow}
+        alt="arrow right"
+        className="arrow right"
+        onClick={() => handleClick("right")}
+      />
+    </div>
+  );
 }
