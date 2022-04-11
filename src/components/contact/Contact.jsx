d504d829c24bb19686d4f0ef8401911c9945c5de
Gabriel Santiago <gsantiagosilva.gs@gmail.com>
2022-04-11 19:42:59 -0300

    salvar projeto

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,25 @@
 import "./contact.scss";
 
+import handShake from "../../assets/handShake.svg";
+
 export function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src={handShake} alt="Hand Shake" />
+      </div>
+      <div className="right">
+        <h2>Contact Me.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
